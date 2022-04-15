585a978d1bef786d19acd712c38894920844585e
Benjamin Gilbert <bgilbert@redhat.com>
2022-04-15 13:25:45 -0400

    Add digest reference to KubeVirt image description
    In the current design, the KubeVirt image is presumed to be a fully-
    qualified reference, including a container digest.  But that implies that
    the user should reference the image by digest, when we'd prefer that they
    reference it by stream-specific tag.
    
    Define the existing "image" field to contain the preferred form of
    fully-qualified reference for the image, which might involve either a
    tag or a digest.  We should still record the unique identifier of the
    current image (as we do for GCP images), so add a "digest-ref" field
    which always contains a fully-qualified reference with digest.

--- tests/it/main.rs
+++ tests/it/main.rs
@@ -1,5 +1,5 @@
 use coreos_stream_metadata::Stream;
-use coreos_stream_metadata::{fcos, SingleImage};
+use coreos_stream_metadata::{fcos, ContainerImage, SingleImage};
 
 const STREAM_DATA: &[u8] = include_bytes!("fixtures/fcos-stream.json");
 
@@ -65,8 +65,9 @@ fn test_basic() {
             .kubevirt
             .as_ref()
             .unwrap(),
-        &SingleImage {
-            image: "quay.io/openshift-release-dev/rhcos@sha256:67a81539946ec0397196c145394553b8e0241acf27b14ae9de43bc56e167f773".to_string(),
+        &ContainerImage {
+            image: "quay.io/openshift-release-dev/rhcos:stable".to_string(),
+            digest_ref: "quay.io/openshift-release-dev/rhcos@sha256:67a81539946ec0397196c145394553b8e0241acf27b14ae9de43bc56e167f773".to_string(),
             release: "33.20201201.3.0".to_string(),
         }
     );
